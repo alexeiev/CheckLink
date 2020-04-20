9a0835078a1f6e70513c3ae94922a8535b29182b
Alexeiev Farias de Araujo <Alexeiev Farias de Araujo>
2020-04-19 23:03:53 -0300

    Finalizada funcao CheckSoftware

--- link.sh
+++ link.sh
@@ -21,6 +21,15 @@
  # MA 02110-1301, USA.
  # 
 
+#Declaracao de Variaveis
+LocalInstall=/opt/CheckLink
+VelocidadeLink=100M
+ETH_WAN=enp0s3
+
+#Variaveis estaticas
+Links=$LocalInstall/url_download.txt
+LogWget=wget-log*
+
 
 function CheckSoftware(){
 	So=$(which apt)
@@ -34,6 +43,7 @@ function CheckSoftware(){
 	Iftop=$(which iftop)
 	#wget
 	Wget=$(which wget)
+	Erro=0
 	if [ -z $Iftop ] && [ -z $Wget ];then
 		echo "O pacote IFTOP e WGET sao necessarios mas nao foram  encontrados, usar o comando $So install iftop wget"
 		Erro=1
@@ -45,25 +55,20 @@ function CheckSoftware(){
 		Erro=1
 	fi
 	[ $Erro -eq 1 ] && exit 1
-	}
 	
+	}
+
 CheckSoftware
-
-LocalInstall=/opt/CheckLink
-
-Links=$LocalInstall/url_download.txt
-LogWget=wget-log*
-ETH_WAN=enp0s3
 cd $LocalInstall
 
 for i in `cat $Links`;do
 
-	wget -b $i -O /dev/nulll
+	wget -b $i -O /dev/nulll > /dev/null 2>&1
 
 done
 
 
-iftop -i $ETH_WAN -n -m100M
+iftop -i $ETH_WAN -n -m$VelocidadeLink
 echo "Matando os downloads"
 killall wget
 echo "Removendo os logs"
